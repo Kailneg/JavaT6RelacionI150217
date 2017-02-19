fb795c24296e90fde806cf422d9ad30a4d28e9f7
cuent <cuent@DESKTOP-MQT6OIV>
2017-02-19 22:37:10 +0100

    Ejercicio 4 terminado

--- src/ejercicio04/Cuenta.java
+++ src/ejercicio04/Cuenta.java
@@ -1,20 +1,40 @@
 package ejercicio04;
 
 public class Cuenta {
-
 	private String numeroCuenta;
-	private double saldoDisponible;
+	private float saldo;
 	
-	public Cuenta(String numeroCuenta, double saldoDisponible) {
+	public Cuenta(String numeroCuenta, float saldo) {
 		this.numeroCuenta = numeroCuenta;
-		this.saldoDisponible = saldoDisponible;
+		this.saldo = saldo;
 	}
 
-	public double consultarSaldo(){
-		return saldoDisponible;
+	public String getNumeroCuenta() {
+		return numeroCuenta;
 	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+	
+	public void recibirAbono(float cantidad){
+		if(cantidad > 0){
+			saldo += cantidad;
+		}
+	}
+	
+	public void pagarRecibo(float cantidad){
+		if(cantidad > 0){
+			saldo -= cantidad;
+		}
+	}	
 	
 	public static String siguienteNumeroCuenta(){
-		return String.valueOf(Math.random());
+		return String.valueOf(Math.round(Math.random()*10000000));
+	}
+
+	@Override
+	public String toString() {
+		return "Cuenta [numeroCuenta=" + numeroCuenta + ", saldo=" + saldo + "]";
 	}
 }
--- src/ejercicio04/Persona.java
+++ src/ejercicio04/Persona.java
@@ -1,39 +1,95 @@
 package ejercicio04;
 
-import java.util.Iterator;
-
 public class Persona {
-	
+	private final int numCuentasMax = 3;
 	private String DNI;
-	private Cuenta[] cuentas = new Cuenta[3];
-	
+	private Cuenta[] cuentas = new Cuenta[numCuentasMax];
+
+	public Persona(String dNI) {
+		DNI = dNI;
+	}
+
 	public Persona(String dNI, Cuenta[] cuentas) {
 		DNI = dNI;
 		this.cuentas = cuentas;
 	}
 	
-	public Persona(String dNI) {
-		DNI = dNI;
+	public void imprimirEstadoCuenta(){
+		for (Cuenta cuenta : cuentas) 
+			if(cuenta != null) 
+				System.out.println("Cuenta " + cuenta.getNumeroCuenta() + " saldo: " + cuenta.getSaldo());
 	}
 	
-	public void nuevaCuenta(float ingresoInicial){
-		String s = Cuenta.siguienteNumeroCuenta();
-		int numeroCuentas = this.numeroCuentas();
-		
-		if (this.numeroCuentas() < 3)
-			cuentas[numeroCuentas] = new Cuenta(s, ingresoInicial);
+	public void imprimirEstadoCuenta(int numCuenta){
+		if(cuentas[numCuenta] != null) 
+			System.out.println("Cuenta " + cuentas[numCuenta].getNumeroCuenta() + " saldo: " + cuentas[numCuenta].getSaldo());
+	}
+	
+	
+	public void recibirDinero(float cantidad, int numCuenta){
+		if(numCuenta < this.numCuentasMax)
+			cuentas[numCuenta].recibirAbono(cantidad);
 		else
-			System.out.println("La persona ya tiene el máximo de cuentas");
-		
+			System.out.println("La cuenta número: " + numCuenta + " no existe.");
+	}
+	
+	public void pagarDinero(float cantidad, int numCuenta){
+		if(numCuenta < this.numCuentasMax)
+			cuentas[numCuenta].pagarRecibo(cantidad);
+		else
+			System.out.println("La cuenta número: " + numCuenta + " no existe.");
+	}
+	
+	public void realizarTransferencia(int numCuenta1, int numCuenta2, float cantidad){
+		pagarDinero(cantidad, numCuenta1);
+		recibirDinero(cantidad, numCuenta2);
 	}
 
-	private int numeroCuentas(){
+	/**
+	 * Comprueba si una persona es morosa o no
+	 * 
+	 * @return true si es morosa, false si no.
+	 */
+	public boolean esMorosa() {
+		boolean esMorosa = false;
+		for (Cuenta c : cuentas) {
+			if (c != null) {
+				if (c.getSaldo() < 0) {
+					esMorosa = true;
+				}
+			}
+		}
+		return esMorosa;
+	}
+
+	public void nuevaCuenta() {
+		this.nuevaCuenta(0);
+	}
+	
+	public void nuevaCuenta(float ingresoInicial) {
+		String numeroCuenta = Cuenta.siguienteNumeroCuenta();
+		int numeroCuentas = this.numeroCuentas();
+		
+		if (this.numeroCuentas() < this.numCuentasMax) {
+			cuentas[numeroCuentas] = new Cuenta(numeroCuenta, ingresoInicial); 
+		} 
+		else {
+			System.out.println("La persona ya tiene el máximo de cuentas");
+		}
+	}
+
+	/**
+	 * Calcula el numero de cuentas creadas de la persona
+	 * 
+	 * @return Numero de cuentas creadas.
+	 */
+	private int numeroCuentas() {
 		int cuentasCreadas = 0;
-		
-		for (Cuenta cuenta : cuentas) 
-			if(cuenta != null)
+		for (Cuenta cuenta : this.cuentas) {
+			if (cuenta != null) {
 				cuentasCreadas++;
-		
+			}
+		}
 		return cuentasCreadas;
 	}
 
